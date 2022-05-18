d2ca3c8d55d195b2cb58aeab4502ec3177c58e40
Formation <you@example.com>
2022-05-18 11:24:06 +0200

    add alert

--- script_imc.js
+++ script_imc.js
@@ -1,28 +1,38 @@
 
 
+function calculer(a, b,) {
+    return a / (b * b);
+}
 
 btn.addEventListener("click", function (e) {
     e.preventDefault()
     let p = document.getElementById("poids").value
-    let t = document.getElementById("taille").value 
+    let t = document.getElementById("taille").value
     let imc = document.getElementById("resultat")
 
-    function calculer(a, b,) {
 
-        console.log(a / (b*b));
-
-
-    }
-
-    console.log(calculer(+p, +t));
-
-
+    result = +p / (+t * +t)
+    console.log('result : ', result)
     resultat.innerHTML = `<p class="text-dark fw-bold"> ${+p / (+t * +t)} </p>`;
 
-
-
-
-
-
+    if (+result <= 18.5) {
+        alert("Attention votre IMC est bas");
+    }
+    else if (+result > 18.5 && +result < 24.9) {
+        alert("Vous êtes de corpulence normale");
+    }
+    else if (+result > 25 && +result < 29.9) {
+        alert("Attention, vous êtes en surpoids")
+    }
+    else if (+result > 30 && +result < 34.9) {
+        alert("Attention, vous êtes en obésité modérée")
+    }
+    else if (+result > 35 && +result < 39.9) {
+        alert("Attention, vous êtes en obésité sévère")
+    }
+    else {
+        alert("Attention, vous êtes en obésité morbide")
+    }
 
 })
+
